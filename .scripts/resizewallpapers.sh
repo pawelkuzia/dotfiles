a969375ee727af5844b9bbca641146b369a6f192
pawelkuzia <pawelkuzia@gmail.com>
2024-07-29 21:56:51 +0200

    minor cleanup

--- .scripts/resizewallpapers.sh
+++ .scripts/resizewallpapers.sh
@@ -8,11 +8,11 @@ cd ~/Downloads/wallpapers || exit
 
 # Przechodzenie przez wszystkie pliki w folderze
 for file in *; do
-    # Sprawdzenie czy plik jest plikiem regularnym
-    if [ -f "$file" ]; then
-        # Przekonwertowanie pliku do formatu PNG o wysokości 2160px z zachowaniem proporcji
-        convert "$file" -resize x2160 -filter Lanczos -quality 90 "$HOME/.wallpapers/${file%.*}.png"
-    fi
+  # Sprawdzenie czy plik jest plikiem regularnym
+  if [ -f "$file" ]; then
+    # Przekonwertowanie pliku do formatu PNG o wysokości 2160px z zachowaniem proporcji
+    convert "$file" -resize x2160 -filter Lanczos -quality 90 "$HOME/.wallpapers/${file%.*}.png"
+  fi
 done
 
 # Przeniesienie wszystkich plików do katalogu backupowego

48e591cd172d514b3cec4e138f1e3cb59e6a197a
pawelkuzia <pawelkuzia@gmail.com>
2024-08-25 23:08:04 +0200

    Minor tweaks

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -8,13 +8,51 @@ return {
       require("catppuccin").setup({
         flavour = "auto", -- latte, frappe, macchiato, mocha
         background = { -- :h background
-            light = "latte",
-            dark = "mocha",
+          light = "latte",
+          dark = "mocha",
         },
         transparent_background = true,
         term_colors = true,
       })
       vim.cmd.colorscheme("catppuccin-mocha")
-    end
-  }
+    end,
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+  },
 }
